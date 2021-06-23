45a5703527e9bf42de832c2ab5cfbaec4d99b605
candidate2609 <keigo0827511@gmail.com>
2021-06-24 05:52:46 +0900

    [add]header responsive

---
+++ assets/js/constants.js
@@ -0,0 +1,28 @@
+export default {
+    menuItems: [
+        {
+            name: 'HOME',
+            url: '#'
+        },
+        {
+            name: 'ABOUT',
+            url: '#'
+        },
+        {
+            name: 'WORK',
+            url: '#'
+        },
+        {
+            name: 'SERVICE',
+            url: '#'
+        },
+        {
+            name: 'BLOG',
+            url: '#'
+        },
+        {
+            name: 'ACCESS',
+            url: '#'
+        }
+    ]
+}
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,74 @@
 <template>
   <div>
-    <v-app-bar color="orange" flat class="title">150th-kaiseifes</v-app-bar>
+    <v-app-bar color="accent-1" light flat>
+      <v-img
+        class="mr-3"
+        src="icon.svg"
+        max-height="40"
+        max-width="40"
+        contain
+      ></v-img>
+      <v-toolbar-title>開成祭</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <div v-if="!($vuetify.breakpoint.xs || $vuetify.breakpoint.sm)">
+        <v-btn text>アクセス</v-btn>
+        <v-btn text>現地企画</v-btn>
+        <v-btn text>お食事処</v-btn>
+        <v-btn text>オンライン企画</v-btn>
+        <v-btn text>お知らせ</v-btn>
+      </div>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+      </v-menu>
+
+      <v-app-bar-nav-icon
+        @click="drawer = true"
+        v-if="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm"
+      ></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-app>
+      <v-navigation-drawer v-model="drawer" fixed temporary>
+        <v-list v-for="(item, i) in Items" :key="i" nav dense>
+          <v-list-item-group>
+            <v-list-item>
+              <v-list-item-title v-text="item" />
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </v-app>
   </div>
 </template>
 
-<style lang="scss" scoped>
-.title {
-  font-family: 'SourceHanSerif-Medium';
+<script>
+import constants from 'assets/js/constants'
+
+export default {
+  data() {
+    return {
+      drawer: false,
+      menuItems: constants.menuItems,
+      Items: ['アクセス', '現地企画', 'お食事処', 'オンライン企画', 'お知らせ'],
+    }
+  },
+}
+</script>
+
+
+<style>
+body,
+.v-application {
+  font-family: 'SourceHanSerif-Regular' !important;
 }
 </style>
