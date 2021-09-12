256e85765a18180bb5294a7b25c02d49954b8a9f
tsym77yoshi <67378554+tsym77yoshi@users.noreply.github.com>
2021-09-12 13:58:37 +0900

    [add]解物成務Header

---
+++ components/KaibutuseimuHeader.vue
@@ -0,0 +1,88 @@
+<template>
+	<div class="kai_title">
+		<div>
+			<img src="icon8header.png" alt="ロゴ" />
+		</div>
+		<h1>解物成務</h1>
+	</div>		
+</template>
+
+<style>
+	body
+	{
+		margin: 0;
+		background-color: #D1F3FF;
+	}
+	.kai_title
+	{
+		height: 3em;
+		background: #8EC0FA;
+		display: flex;
+		justify-content: center; 
+		
+	}
+	.kai_title h1
+	{
+		margin-left: 1em;
+		
+		margin-top: auto;
+		margin-bottom: auto;
+		text-align: center;
+		text-shadow: 0.04em 0.04em 0em #000000, -0.04em -0.04em 0em #000000, -0.04em 0.04em 0em #000000,0.04em -0.04em 0em #000000,0em 0.04em 0em #000000,0em -0.04em 0em #000000,0.04em 0em 0em #000000,-0.04em 0em 0em #000000;
+		color: white;
+	}
+	.kai_title img
+	{
+		margin-top: auto; 
+		margin-bottom: auto; 
+		height: 100%; 
+		width: auto; 	
+	}
+	.button
+	{
+		margin-top: 0.5em;
+		text-align: center;
+	}
+			
+	.button a
+	{
+		text-shadow: 0.04em 0.04em 0em #000000, -0.04em -0.04em 0em #000000, -0.04em 0.04em 0em #000000,0.04em -0.04em 0em #000000,0em 0.04em 0em #000000,0em -0.04em 0em #000000,0.04em 0em 0em #000000,-0.04em 0em 0em #000000;
+        position: relative;
+        display: inline-block;
+        padding: 0.25em 2em;
+        text-decoration: none;
+                color: #FFF;
+                background: #62C1FA;/*背景色*/
+                border-bottom: solid 0.2em #4D82E3;/*少し濃い目の色に*/
+                border-radius: 0.4em;/*角の丸み*/
+                box-shadow: inset 0 0.2em 0 rgba(255,255,255,0.2), 0 0.2em 0.2em rgba(0, 0, 0, 0.19);
+                font-weight: bold;
+			}
+
+            .button a:active 
+			{
+                border-bottom: solid 0.2em #fd9535;
+                box-shadow: 0 0 0.2em rgba(0, 0, 0, 0.30);
+			}
+			.story
+			{
+				margin: auto;
+				text-align: center;
+				width: 80%;
+                padding: 0.5em;
+                border-radius: 1em;
+                border: dashed #688C9B;
+                box-shadow: 0 0 0 1em #D1F3FF;
+				position: relative;
+			}
+	.story::before 
+	{
+		background-color: #D1F3FF;
+    content: "ストーリー";
+    font-weight: bold;
+    position: absolute;
+                padding: 0 1em;
+                left: 1em;
+                top: -1em;
+  }
+</style>
