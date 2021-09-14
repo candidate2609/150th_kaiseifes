e7a6f29730cf733c13d23cd1a93de3861ea246ca
tsym77yoshi <67378554+tsym77yoshi@users.noreply.github.com>
2021-09-14 21:22:50 +0900

    [change]h1を修正

--- pages/kaibutuseimu/clear.vue
+++ pages/kaibutuseimu/clear.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<h1 style="text-align: center;">景品ページ</h1>
+		<h1 style="text-align: center;" class="kbsmh1">景品ページ</h1>
 		<p style="text-align: center;">景品は、創立150年を記念して建てられた高校新校舎の写真です</p>
 		<div class="button" style="text-align: center;">
 			<a href="./">スマホの壁紙 >></a>
