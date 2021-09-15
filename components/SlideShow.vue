f5d347ea5c08b16c92f3c1fbf2a7cc3bfd4ec8f2
candidate2609 <keigo0827511@gmail.com>
2021-09-15 12:14:25 +0900

    [update]pageant carousel delimiter-background

--- components/SlideShow.vue
+++ components/SlideShow.vue
@@ -1,11 +1,5 @@
 <template>
-  <v-carousel
-    class="carousel"
-    height="50vh"
-    :show-arrows="false"
-    :hide-delimiter-background="true"
-    :cycle="true"
-  >
+  <v-carousel class="carousel" height="50vh" :show-arrows="false" :cycle="true">
     <v-carousel-item v-for="(item, i) in imgs" :key="i">
       <div style="display: flex; justify-content: center; height: 100%">
         <img :src="item.src" alt="" style="max-height: 100%" width="auto" />
