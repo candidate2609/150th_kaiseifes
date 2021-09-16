8b7b59d287251e3e81219ab859d003744a886184
HiroSpark <73699844+HiroSpark@users.noreply.github.com>
2021-09-16 17:48:05 +0900

    [update] notify section

--- components/Notify.vue
+++ components/Notify.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="content">
-    <p v-for="(item, index) in data.news" :key="index">
-      <span id="time">{{ item.datetime.slice(0, 10) }}</span>
-      <span id="tag">{{ data.tags[item.tag] }}</span>
-      <br v-if="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm" />
-      <span id="title">{{ item.title }}</span>
-    </p>
-  </div>
+  <section>
+    <h2>現在開催中のイベント</h2>
+    <h2>まもなく始まるイベント</h2>
+    <h2>新しいコンテンツ</h2>
+    <ul class="new-content">
+      <li v-for="(item, index) in data.news" :key="index">
+        <span id="title">{{ item.title }}</span>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
@@ -18,23 +20,7 @@ export default {
 </script>
 
 <style scoped>
-#content p {
-  margin-left: 10vw;
-  margin-top: 2vh;
-  padding-bottom: 2vh;
-  border-bottom: 1px solid #f6f6f6;
-}
-
-@media screen and (min-width: 600px) {
-  #content p {
-    margin-left: 30vw;
-  }
-}
-
-#tag {
-  padding: 4px;
-  margin: 0 1vw;
-  background-color: #bf9547;
-  color: white;
+h2 {
+  font-weight: 800;
 }
 </style>
