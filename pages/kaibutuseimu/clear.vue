1df6ccc1a52b5a270fc0e2d68efab57b5b697f52
tsym77yoshi <67378554+tsym77yoshi@users.noreply.github.com>
2021-09-18 02:33:10 +0900

    [change]clear

--- pages/kaibutuseimu/clear.vue
+++ pages/kaibutuseimu/clear.vue
@@ -3,6 +3,8 @@
 		<h1 style="text-align: center;" class="kbsmh1">景品ページ</h1>
 		<p style="text-align: center;">景品は、創立150年を記念して建てられた高校新校舎の写真です</p>
 		<div class="button" style="text-align: center;">
+			<img src="clear.png" style="width:80%"/>
+			<br>
 			<a href="./">スマホの壁紙 >></a>
 		</div>	
 		
